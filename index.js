7eb11dfc60043378b6bdd615742aff285c088dec
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-13 01:30:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-var kittens = [] //define your array here
+var kittens = ["Milo" , "Otis" , "Garfield"] //define your array here
 
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.pop(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten()
+{
+  
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  
+}
+
+function appendKitten(name)
+{
+  
+}
+
+function prependKitten(name)
+{
+  
+}
+
+function removeLastKitten()
+{
+  
+}
+
+function removeFirstKitten()
+{
+  
+}
 // Add your functions and code here
